79e08e27fc0595b55a8992056b73f694621441b2
Fran Borg <francesca.bte@outlook.com>
2019-09-05 11:28:01 +0200

    Add aspect ration functionality

---
+++ modules/aspect-ratio.js
@@ -0,0 +1,18 @@
+function calculateAspectRatio(originalWidth, originalHeight, newValues) {
+    let resultWidth = newValues[0];
+    let resultHeight = newValues[1];
+  
+    if ((originalWidth > 0) && (originalHeight > 0)) {
+        newValues.forEach(newValue => {
+            if ((newValue.classList.contains("height")) && (newValue.value > 0)) {
+                return resultWidth.value = (originalWidth / originalHeight) * newValue.value;
+                
+            } else if ((newValue.classList.contains("width")) && (newValue.value > 0)) {
+                newValue.style.outline = "pink";
+                return resultHeight.value = (originalHeight / originalWidth) * newValue.value;
+            }
+        })
+    }
+}
+
+export {calculateAspectRatio};
